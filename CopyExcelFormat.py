9c211f4f6865fd70e43da40f8fd756d96f04e5a7
rochismandatta <45571017+rochismandatta@users.noreply.github.com>
2018-12-03 23:47:51 +0530

    Delete CopyExcelFormat.py

--- CopyExcelFormat.py
+++
@@ -1,9 +0,0 @@
-import shutil,sys
-def CopyExcel(storecode):
-    
-    for i in storecode:
-        shutil.copy("C:/Users/rochisman.datta/Desktop/Python/Excel Macros/template_formal.xlsx", "C:/Users/rochisman.datta/Desktop/Python/Excel Macros/{}_formal.xlsx".format(i))
-        shutil.copy("C:/Users/rochisman.datta/Desktop/Python/Excel Macros/template_casual.xlsx", "C:/Users/rochisman.datta/Desktop/Python/Excel Macros/{}_casual.xlsx".format(i))
-
-
-
