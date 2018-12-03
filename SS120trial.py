6c91c1cc71a10025f75e10b51dd3d7b44fc58872
rochismandatta <45571017+rochismandatta@users.noreply.github.com>
2018-12-03 23:50:13 +0530

    Delete SS120trial.py

--- SS120trial.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df=pd.read_excel('C:/Users/rochisman.datta/Desktop/Python/Data_DS/Pantaloons Diagnostic fill data sheets/Copy of DS_SS18_120 Days Sell Thru_BASE FILE.xlsx', sheet_name='Base Data')
-df.set_index('Store Code', inplace = True)
-df1 = df.pivot_table(index = ['Store Code','Product'],columns = ['Category','Fabric Design Type'],values = ['Sales.', 'Dispatch'],aggfunc= 'sum')
-
-print(df.head())
-df1.loc[2048].to_excel('TestingSS120.xlsx')
