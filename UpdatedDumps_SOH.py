72a3f9d1a1326c73f938bf91cce0ca6d5c173116
rochismandatta <45571017+rochismandatta@users.noreply.github.com>
2018-12-03 23:51:00 +0530

    Delete UpdatedDumps_SOH.py

--- UpdatedDumps_SOH.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-df= pd.read_excel('C:/Users/rochisman.datta/Desktop/Data_DS/Updated Dumps/SOH Feb Mar Apr May.xlsx',sheet_name='Sheet1')
-df1 = df.pivot_table(index = 'Store',columns = ['Brand ','Month','Product'], aggfunc='sum')
-df1.loc[2404].to_excel('2404_SOH_updated.xlsx')
-##print(df1.loc[2404])
