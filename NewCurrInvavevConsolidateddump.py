ac62b8790d8fab364244cb9e19944101de5d06a2
rochismandatta <45571017+rochismandatta@users.noreply.github.com>
2018-12-03 23:49:24 +0530

    Delete NewCurrInvavevConsolidateddump.py

--- NewCurrInvavevConsolidateddump.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-def Curr_Inv(i):
-    df= pd.read_excel('C:/Users/rochisman.datta/Desktop/Python/Data_DS/Pantaloons Diagnostic fill data sheets/Updated Dumps/Consolidatin/consolidated_CurrInv_Aug.xlsx',sheet_name='Sheet1')
-    df1 = df.pivot_table(index = 'Site',columns = ['Brand 2','Product'], aggfunc='sum')
-    print(df1.head())
-    df1.loc[i].fillna(0).to_excel('C:/Users/rochisman.datta/Desktop/Python/Python code/CurrInv collection/CurrInv_{}.xlsx'.format(i))
-
-Curr_Inv(2097)
